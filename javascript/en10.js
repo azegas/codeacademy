1d63d580183c80d00c2e599a113ffda596c54785
arvydasg <azegaspa@gmail.com>
2022-09-05 20:26:56 +0300

    feat: added fully working celsius to fahrenheit converter

--- javascript/en10.js
+++ javascript/en10.js
@@ -1,6 +1,31 @@
-celsius = 50
+const celsiusForm = document.getElementById("celsiusForm");
+const celsiusInput = document.getElementById("celsiusInput");
+const celsiusResult = document.getElementById("celsiusResult");
 
-const farenheitCalculation = (celsius)=>{
-    const result = (celsius * 9/5) + 32;
-    console.log(result)
+function celsiusToFconverter(b) {
+    b.preventDefault()
+    const receivedCelsius = Number(celsiusInput.value);
+    // console.log(receivedCelsius)
+
+    const resultInFahrenheit = (receivedCelsius * 9/5) +32;
+    celsiusResult.textContent = receivedCelsius + "°C " + " is " + resultInFahrenheit + "°F."
 }
+
+celsiusForm.addEventListener("submit", celsiusToFconverter);
+
+//
+
+const fahrenheitForm = document.getElementById("fahrenheitForm");
+const fahrenheitInput = document.getElementById("fahrenheitInput");
+const fahrenheitResult = document.getElementById("fahrenheitResult");
+
+function fahrenheitToCconverter(b) {
+    b.preventDefault()
+    const receivedFahrenheit = Number(fahrenheitInput.value);
+    console.log(receivedFahrenheit)
+
+    const resultInCelsius = (receivedFahrenheit - 32) / (9/5);
+    fahrenheitResult.textContent = receivedFahrenheit + "°F " + " is " + resultInCelsius + "°C."
+}
+
+fahrenheitForm.addEventListener("submit", fahrenheitToCconverter);
