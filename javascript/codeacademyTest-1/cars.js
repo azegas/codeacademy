35c278a755568d22ab8201aa97889f26ef1dd8da
arvydasg <azegaspa@gmail.com>
2022-10-10 06:28:06 +0300

    refactor: simply renamed the files

---
+++ javascript/codeacademyTest-1/cars.js
@@ -0,0 +1,37 @@
+// smaller file
+
+const cars = [
+  {
+    Name: "chevrolet chevelle concours (sw)",
+    Miles_per_Gallon: null,
+    Cylinders: 8,
+    Displacement: 350,
+    Horsepower: 165,
+    Weight_in_lbs: 4142,
+    Acceleration: 11.5,
+    Year: "1970-01-01",
+    Origin: "USA",
+  },
+  {
+    Name: "ford torino (sw)",
+    Miles_per_Gallon: null,
+    Cylinders: 8,
+    Displacement: 351,
+    Horsepower: 153,
+    Weight_in_lbs: 4034,
+    Acceleration: 11,
+    Year: "1970-01-01",
+    Origin: "USA",
+  },
+  {
+    Name: "plymouth satellite (sw)",
+    Miles_per_Gallon: null,
+    Cylinders: 8,
+    Displacement: 383,
+    Horsepower: 175,
+    Weight_in_lbs: 4166,
+    Acceleration: 10.5,
+    Year: "1970-01-01",
+    Origin: "USA",
+  },
+];    
