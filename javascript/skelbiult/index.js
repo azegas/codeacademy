c6d578d4e7228d733b9e8d565bb6e10064ed340e
arvydasg <azegaspa@gmail.com>
2022-10-12 20:45:46 +0300

    feat: adding a second fetch and other tweaks

--- javascript/skelbiult/index.js
+++ javascript/skelbiult/index.js
@@ -1,79 +1,135 @@
 const END_POINT = "https://63443d16dcae733e8fdaf088.mockapi.io/produktai"
 
 const dataArr = {};
+const dataArr2 = {};
 
 const setId = (id) => {
     localStorage.setItem("itemId", id);
 };
 
-const createCard = (object) => {
+const createSuggestionCard = (object) => {
     // defining wrappers
-    const cardWrapper = document.getElementById("cardWrapper");
+    const cardWrapper = document.getElementById("items");
 
     // defining elements
     const card = document.createElement("div");
     const cardImage = document.createElement("img");
     const cardBody = document.createElement("div")
     const cardTitle = document.createElement("h5");
-    const cardButton = document.createElement("a")
-    const cardPrice = document.createElement("p");
+    const cardOverlay = document.createElement("div")
+    const cardLink = document.createElement("a")
 
     // adding styles to the elements
     card.classList.add("card");
-    card.style.width = "18rem"
 
-    cardButton.classList.add("btn")
-    cardButton.classList.add("btn-primary")
-    
-    cardImage.classList.add("card-img-top")
     cardImage.setAttribute("src", object.nuotrauka)
-    cardImage.style.height = "200px"
-    
-    cardBody.classList.add("card-body")
+    cardImage.classList.add("card-img")
     
     cardTitle.classList.add("card-title")
-    cardTitle.style.color = "red"
-    cardTitle.style.fontWeight = "800"
-    
-    cardPrice.classList.add("card-text")
-    
-    cardButton.classList.add("btn")
-    cardButton.classList.add("btn-primary")
-    cardButton.innerHTML = "Go somewhere"
 
+    cardBody.classList.add("card-body")
+    cardOverlay.classList.add("overlay")
+
+    cardLink.classList.add("cardLink") // overlays the whole card, acts as a clickable link
+    
     // adding content
     cardTitle.innerHTML = object.pavadinimas
-    cardPrice.innerText = object.kaina + " $"
-    cardButton.innerHTML = "button";
-    cardButton.href = "item.html";
+    cardLink.href = "item.html";
 
     // appending
     cardWrapper.append(card);
-    card.append(cardImage, cardBody);
-    cardBody.append(cardPrice, cardTitle, cardButton);
+    card.append(cardBody);
+    cardBody.append(cardImage, cardTitle, cardOverlay, cardLink);
 
     // on button click add its id to localstorage
-    cardButton.addEventListener("click", () => {
+    cardBody.addEventListener("click", () => {
         setId(object.id);
     });
-};    
+};
+
+const createCard = (object) => {
+    // defining wrappers
+    const cardWrapper = document.getElementById("items2");
+
+    // defining elements
+    const card = document.createElement("div");
+    const cardInfo = document.createElement("div");
+    const cardReview = document.createElement("div");
+    const cardImageWrapper = document.createElement("div");
+    const cardImage = document.createElement("img");
+    const cardPrice = document.createElement("p");
+    const cardTitle = document.createElement("a");
+    const cardCity = document.createElement("p");
+    const cardDescription = document.createElement("p");
+
+    // adding styles to the elements
+    card.classList.add("card2");
+    cardInfo.classList.add("itemInfo");
+    cardReview.classList.add("itemReview");
+    cardPrice.classList.add("price");
+    cardDescription.classList.add("description");
+    
+    cardImageWrapper.classList.add("itemImage")
+
+    cardCity.classList.add("itemInfo")
+    cardTitle.classList.add("title2")
+
+    cardImage.setAttribute("src", object.nuotrauka)
+    
+    // adding content
+    cardTitle.innerHTML = object.pavadinimas
+    cardCity.innerHTML = object.miestas
+    cardDescription.textContent = object.aprasymas
+    cardPrice.innerText = object.kaina + " €"
+    cardTitle.href = "item.html";
+
+    // appending
+    cardWrapper.append(card);
+    card.append(cardInfo, cardImageWrapper, cardReview)
+    cardInfo.append(cardCity)
+    cardImageWrapper.append(cardImage)
+    cardReview.append(cardTitle, cardDescription, cardPrice)
+
+    // on button click add its id to localstorage
+    cardTitle.addEventListener("click", () => {
+        setId(object.id);
+    });
+};
 
 
-const fetchData = async () => {
+const fetchDataAscending = async () => {
     try {
         const response = await fetch(END_POINT);
         if (response.ok) {
             dataArr.anything = await response.json();
             const filterLowtoHigh = dataArr.anything.sort(function (a,b){
-                return a.kaina - b.kaina
+                return a.kaina + b.kaina
             })
             console.log(filterLowtoHigh)
 
-            dataArr.anything.forEach((x) => createCard(x));
+            dataArr.anything.forEach((x) => createSuggestionCard(x));
         }
     } catch (error) {
         console.error(error);
     }
 };
 
-fetchData()
+const fetchDataRandom = async () => {
+    try {
+        const response2 = await fetch(END_POINT);
+        if (response2.ok) {
+            dataArr2.labas = await response2.json();
+            const filterHighToLow = dataArr2.labas.sort(function (z,x){
+                return z.kaina - x.kaina
+            })
+            console.log(filterHighToLow)
+
+            dataArr2.labas.forEach((f) => createCard(f));
+        }
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+fetchDataAscending()
+fetchDataRandom()
