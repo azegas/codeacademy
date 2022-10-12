fa853a0fc146cec851a8cfd27f62a5811c293645
arvydasg <azegaspa@gmail.com>
2022-10-12 20:44:56 +0300

    feat: changing the language from english to lithuanian

--- javascript/skelbiult/add.js
+++ javascript/skelbiult/add.js
@@ -24,15 +24,16 @@ const postData = async (object) => {
             },
             body: JSON.stringify(object),
         });
-        if (response.status === 500) {
+        // 400 Bad Request - server cannot process, client error
+        if (response.status === 400) {
             statusMsg.innerHTML = "";
-            statusMsg.style.color = "#03d3b2";
-            statusMsg.innerHTML = `Please check if all mandatory data provided. Here is response status code: ${response.status}`;
+            statusMsg.style.color = "red";
+            statusMsg.innerHTML = `Įsitikinkite, jog visi privalomi laukai užpildyti teisingai. Klaidos kodas: ${response.status}`;
         } else if (response.ok) {
             statusMsg.innerHTML = "";
-            statusMsg.style.color = "black";
-            statusMsg.innerHTML = "Data successfully sent! Returning to home page...";
-            setTimeout(clickToHome, 1000);
+            statusMsg.style.color = "green";
+            statusMsg.innerHTML = "Prekė ikelta sėkmingai! Būsite nukreipti į pagrindinį puslapį...";
+            setTimeout(clickToHome, 3000);
         }
     } catch (error) {
         statusMsg.innerHTML = "";
@@ -50,21 +51,21 @@ const validateForm = () => {
     const cityErrElement = document.querySelector("#city-block span");
     
     if (objectName.value == "") {
-        nameErrElement.innerHTML = "Name field must be correctly filled";
+        nameErrElement.innerHTML = "Pavadinimo laukas turi būti užpildytas teisingai.";
         nameErrElement.style.visibility = "visible";
         validationOk = false;
     } else {
         nameErrElement.style.visibility = "hidden";
     }
     if (objectName.value == "") {
-        priceErrElement.innerHTML = "Price field must be correctly filled";
+        priceErrElement.innerHTML = "Kainos laukas turi būti užpildytas teisingai.";
         priceErrElement.style.visibility = "visible";
         validationOk = false;
     } else {
         priceErrElement.style.visibility = "hidden";
     }
     if (objectImage.value == "" || !objectImage.value.includes("http")) {
-        imageErrElement.innerHTML = "Image URL field must be correctly filled";
+        imageErrElement.innerHTML = "Nuotraukos laukas(URL) turi būti užpildytas teisingai.";
         imageErrElement.style.visibility = "visible";
         validationOk = false;
     }
@@ -72,14 +73,14 @@ const validateForm = () => {
         imageErrElement.style.visibility = "hidden";
     }
     if (objectName.value == "") {
-        descriptionErrElement.innerHTML = "Description field must be correctly filled";
+        descriptionErrElement.innerHTML = "Aprašymo laukas turi būti užpildytas teisingai.";
         descriptionErrElement.style.visibility = "visible";
         validationOk = false;
     } else {
         descriptionErrElement.style.visibility = "hidden";
     }
     if (objectName.value == "") {
-        cityErrElement.innerHTML = "City field must be correctly filled";
+        cityErrElement.innerHTML = "Miesto laukas turi būti užpildytas teisingai.";
         cityErrElement.style.visibility = "visible";
         validationOk = false;
     } else {
