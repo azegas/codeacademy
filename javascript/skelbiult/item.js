cfeba3b84dab3f71c38d745234cab37161952424
arvydasg <azegaspa@gmail.com>
2022-10-12 20:46:17 +0300

    feat: lots of tweaks, kind of finished

--- javascript/skelbiult/item.js
+++ javascript/skelbiult/item.js
@@ -30,66 +30,75 @@ const deleteItem = (id) => {
 
 const createCard = (object) => {
     // defining wrappers
-    const cardWrapper = document.getElementById("cardWrapper");
+    const cardWrapper = document.getElementById("content")
 
     // defining elements
-    const card = document.createElement("div");
+    const cardLeft = document.createElement("div");
+    const cardRight = document.createElement("div");
+    const cardRightImage = document.createElement("img");
+    const cardTitleBlock = document.createElement("div");
+    const cardTitleBlockLeft = document.createElement("div");
+    const cardTitleBlockRight = document.createElement("div");
+    const cardDescription = document.createElement("div");
     const cardImage = document.createElement("img");
-    const cardBody = document.createElement("div")
     const cardTitle = document.createElement("h5");
-    const cardDescription = document.createElement("p");
+    const cardPrice = document.createElement("p");
     const cardCity = document.createElement("p");
-    const cardButton = document.createElement("button")
-    const cardText = document.createElement("p");
-    const cardDelete = document.createElement("button")
+    const cardDelete = document.createElement("button");
 
     // adding styles to the elements
-    card.classList.add("card");
-    card.style.width = "18rem"
+    cardLeft.classList.add("left-content");
+    cardRight.classList.add("right-content")
+    cardTitleBlock.classList.add("card-title-block")
+    cardTitleBlockLeft.classList.add("card-title-block-left")
+    cardTitleBlockRight.classList.add("card-title-block-right")
+    cardCity.classList.add("product-city")
+    cardPrice.classList.add("product-price")
+    cardDescription.classList.add("product-description")
 
     cardDelete.classList.add("delete")
     
-    cardButton.classList.add("btn")
-    cardButton.classList.add("btn-primary")
+    // cardButton.classList.add("btn")
+    // cardButton.classList.add("btn-primary")
     
-    cardImage.classList.add("card-img-top")
+    cardImage.classList.add("product-image")
     cardImage.setAttribute("src", object.nuotrauka)
-    cardImage.style.height = "200px"
+    cardRightImage.setAttribute("src", "./references/sidebar.png")
     
-    cardBody.classList.add("card-body")
+    // cardBody.classList.add("card-body")
     
-    cardTitle.classList.add("card-title")
-    cardTitle.style.color = "red"
-    cardTitle.style.fontWeight = "800"
+    cardTitle.classList.add("product-title")
     
-    cardButton.classList.add("btn")
-    cardButton.classList.add("btn-primary")
-    cardButton.innerHTML = "Go somewhere"
+    // cardButton.classList.add("btn")
+    // cardButton.classList.add("btn-primary")
+    // cardButton.innerHTML = "Go somewhere"
 
     // adding content
-    cardTitle.innerHTML = "Pavadinimas: " + object.pavadinimas
-    cardDescription.innerHTML = "Aprasymas: " + object.aprasymas
-    cardText.innerText = "Kaina: " + object.kaina
-    cardCity.innerText = "Miestas: " + object.miestas
-    cardDelete.innerText = "x"
-    cardButton.innerHTML = "pirkti";
+    cardTitle.innerHTML = object.pavadinimas
+    cardDelete.innerHTML = "Ištrinti"
+    cardPrice.innerText = object.kaina + " €"
+    cardCity.innerText = object.miestas
+    cardDescription.innerText = object.aprasymas
 
     // appending
-    cardWrapper.append(card);
-    card.append(cardImage, cardBody);
-    cardBody.append(cardText, cardTitle, cardDescription, cardCity, cardButton, cardDelete);
-
+    cardWrapper.append(cardLeft, cardRight);
+    cardRight.append(cardRightImage);
+    cardLeft.append(cardImage, cardTitleBlock, cardDescription, cardDelete);
+    cardTitleBlock.append(cardTitleBlockLeft, cardTitleBlockRight)
+    cardTitleBlockLeft.append(cardTitle, cardCity)
+    cardTitleBlockRight.append(cardPrice)
+    
     // on button click - remove element
     cardDelete.addEventListener("click", () => {
         deleteItem(object.id);
-        alert("PREKE Istrinta, mldc")
+        setTimeout(function() { alert("Preke ištrinta. Būsite nukreipti į pagrindinį puslapį"); }, 2000);
         setTimeout(clickToHome, 2000)
     });
 
     // on button click add its id to localstorage
-    cardButton.addEventListener("click", () => {
-        setId(object.id);
-    });
+    // cardButton.addEventListener("click", () => {
+    //     setId(object.id);
+    // });
 };    
 
 const fetchData = async () => {
